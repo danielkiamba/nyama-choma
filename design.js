f8a8d861cf281acda8ac93b12f888e19bc0e336f
Daniel Kiamba <treydancan@gmail.com>
2025-02-26 11:07:44 +0300

    add changes

--- design.js
+++ design.js
@@ -3,10 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let myButton = document.getElementById('show-more');
 
     myButton.addEventListener('click', function() {
-<<<<<<< HEAD
         event.preventDefault
-=======
->>>>>>> 127c3af0ee1570f87f9aec2e761141865a179f15
         if (myContent.classList.contains('open')) {
             // shrink the box
             myContent.classList.remove('open');
@@ -16,7 +13,6 @@ document.addEventListener('DOMContentLoaded', function() {
             myContent.classList.add('open');
             myButton.innerHTML = 'Show Less';
         }
-<<<<<<< HEAD
     })
 }); 
 document.addEventListener('DOMContentLoaded', function() {
@@ -35,13 +31,3 @@ if(x.classList.contains(`opena`)) {
 })
 
 
-=======
-    });
-});
-
-$(document).ready(function() {
-    $(`.burger-nav`).on(`click`, function() {
-        $(`header nav ul`).toggleClass(`open`);
-    })
-});
->>>>>>> 127c3af0ee1570f87f9aec2e761141865a179f15
