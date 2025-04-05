212ec1d5163582c4faaeabed9683bc0a97ddac3b
Daniel Kiamba <treydancan@gmail.com>
2025-04-05 20:02:43 +0300

    new changes

--- news.js
+++
@@ -1,8 +0,0 @@
-const accordionItem = document.querySelector(`.accordion-item`);
-const accordionContent = document.querySelector(`.accordion-content`);
-
-accordionItem.addEventListener(`click`, () => {
-
-accordionContent.classList.toggle(`active`);
-
-})
